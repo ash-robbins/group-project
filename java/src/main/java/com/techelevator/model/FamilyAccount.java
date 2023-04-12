133c3c97408f7da971b5967054c56fd8cc498e06
Yae Lee <peteryaelee@gmail.com>
2023-04-12 15:53:35 -0400

    POJOs for familyMember, familyAccount, Books

---
+++ java/src/main/java/com/techelevator/model/FamilyAccount.java
@@ -0,0 +1,59 @@
+package com.techelevator.model;
+
+import java.time.LocalDate;
+
+public class FamilyAccount {
+    private int familyId;
+    private String familyName;
+    private int createdBy;
+    private LocalDate createdDate;
+
+    public FamilyAccount(int familyId, String familyName, int createdBy, LocalDate createdDate) {
+        this.familyId = familyId;
+        this.familyName = familyName;
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+    }
+
+    public int getFamilyId() {
+        return familyId;
+    }
+
+    public void setFamilyId(int familyId) {
+        this.familyId = familyId;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "FamilyAccount{" +
+                "familyId=" + familyId +
+                ", familyName='" + familyName + '\'' +
+                ", createdBy=" + createdBy +
+                ", createdDate=" + createdDate +
+                '}';
+    }
+}
---
+++ java/src/main/java/com/techelevator/model/FamilyMember.java
@@ -0,0 +1,46 @@
+package com.techelevator.model;
+
+public class FamilyMember {
+    private int familyId;
+    private int userId;
+    private String memberType;
+
+    public FamilyMember(int familyId, int userId, String memberType) {
+        this.familyId = familyId;
+        this.userId = userId;
+        this.memberType = memberType;
+    }
+
+    public int getFamilyId() {
+        return familyId;
+    }
+
+    public void setFamilyId(int familyId) {
+        this.familyId = familyId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
+    }
+
+    @Override
+    public String toString() {
+        return "FamilyMember{" +
+                "familyId=" + familyId +
+                ", userId=" + userId +
+                ", memberType='" + memberType + '\'' +
+                '}';
+    }
+}
