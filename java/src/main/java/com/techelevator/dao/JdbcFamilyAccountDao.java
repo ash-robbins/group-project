b1098264b030890d0a7cc2369e67ec2bedaaa19f
Yae Lee <peteryaelee@gmail.com>
2023-04-13 18:44:53 -0400

    createFamilyAccount method sql

--- java/src/main/java/com/techelevator/dao/JdbcFamilyAccountDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcFamilyAccountDao.java
@@ -1,13 +1,25 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.FamilyAccount;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class JdbcFamilyAccountDao implements FamilyAccountDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcFamilyAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public FamilyAccount createFamilyAccount(String familyName, int createdBy) {
-        return null;
+        String sql = "INSERT INTO family_account (family_name, created_by) " +
+                     "VALUES (?, ?)"+
+                     "RETURNING family_id;";
+        Integer newId = jdbcTemplate.queryForObject(sql, Integer.class, familyName, createdBy);
+        return getFamilyAccountById(newId);
     }
 
     @Override
