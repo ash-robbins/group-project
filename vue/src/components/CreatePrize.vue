f47b908a4950b24b1c7b814357d7498a9fabba70
Mohammed <mnaser11218@gmail.com>
2023-04-14 15:14:30 +0300

    set prize component

---
+++ vue/src/components/CreatePrize.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      
+        <button v-if="updateButton" v-on:click.prevent="updateButton = false">Set new Prize</button>
+        <form v-else v-on:submit.prevent="createNewPrize">
+          <label>Prize description</label>
+          <input type="text" placeholder="Prize description" v-model="prize.description"/>
+          <label>Start Date</label>
+          <input type="date" v-model="prize.start_date"/>
+           <label>End Date</label>
+          <input type="date" v-model="prize.end_date"/>
+          <input type="submit" value="Submit"/>
+        </form>
+      
+
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'create-prize',
+    data(){
+        return{
+            updateButton: true,
+            prize:{
+                description: '',
+                start_date: '',
+                end_date: ''
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Prizes.vue
+++ vue/src/views/Prizes.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
       You're in the Prizes Component
+      <create-prize/>
   </div>
 </template>
 
 <script>
+import CreatePrize from '../components/CreatePrize.vue'
 export default {
-
+name: 'prizes',
+components: {
+  CreatePrize
+}
 }
 </script>
 
