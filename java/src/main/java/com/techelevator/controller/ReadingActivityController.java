b78a323574f68182c9857aa5ba2bc382b9978809
ashley robbins <ashley.robbins22@aol.com>
2023-04-14 15:33:03 -0400

    Controller updates?? Need correct endpoints

--- java/src/main/java/com/techelevator/controller/ReadingActivityController.java
+++ java/src/main/java/com/techelevator/controller/ReadingActivityController.java
@@ -1,6 +1,10 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.PrizeDao;
+import com.techelevator.dao.PrizeWinnerDao;
 import com.techelevator.dao.ReadingActivityDao;
+import com.techelevator.model.Prize;
+import com.techelevator.model.PrizeWinner;
 import com.techelevator.model.ReadingActivity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,19 +17,24 @@ import org.springframework.web.server.ResponseStatusException;
 public class ReadingActivityController {
 
     private ReadingActivityDao readingActivityDao;
+    private PrizeDao prizeDao;
+    private PrizeWinnerDao prizeWinnerDao;
 
-    public ReadingActivityController(ReadingActivityDao readingActivityDao) {
+    public ReadingActivityController(ReadingActivityDao readingActivityDao, PrizeDao prizeDao, PrizeWinnerDao prizeWinnerDao) {
         this.readingActivityDao = readingActivityDao;
+        this.prizeDao = prizeDao;
+        this.prizeWinnerDao = prizeWinnerDao;
     }
 
     /**
+     *
      *
      * Return Reading Activity
      * @param id reading activity id
      * @return reading activity information
      */
     @RequestMapping(path = "/reading_activity/{id}", method = RequestMethod.GET)
-    public ReadingActivity get(@PathVariable int id) {
+    public ReadingActivity getActivity(@PathVariable int id) {
         ReadingActivity readingActivity = readingActivityDao.getReadingActivity(id);
             if (readingActivity == null) {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to locate Reading Activity.");
@@ -34,4 +43,54 @@ public class ReadingActivityController {
             }
         }
 
+    /**
+     *
+     *
+     * Return Reading Activity By Book
+      * @param bookId book id
+     * @return reading information
+     */
+     @RequestMapping(path = "/reading_activity/{id}", method = RequestMethod.GET)
+     public ReadingActivity getActivityByBook(@PathVariable int bookId) {
+        ReadingActivity readingActivity = readingActivityDao.getReadingActivityByBookId(bookId);
+        if (readingActivity == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to locate Reading Activity for this Book.");
+        } else {
+            return readingActivity;
+        }
+     }
+
+    /**
+     *
+     *
+     * Return Prize
+      * @param id prize id
+     * @return prize information
+     */
+    @RequestMapping(path = "/prize/{id}", method = RequestMethod.GET)
+    public Prize getPrize(@PathVariable int id) {
+        Prize prize = prizeDao.getPrize(id);
+        if (prize == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Prize not found.");
+        } else {
+            return prize;
+        }
+    }
+
+    /**
+     *
+     *
+     * @param id prize winner id
+     * @return prize winner information
+     */
+
+    @RequestMapping(path = "/prize_winner{id}", method = RequestMethod.GET)
+    public PrizeWinner getWinner(@PathVariable int id) {
+        PrizeWinner prizeWinner = prizeWinnerDao.getPrizeWinner(id);
+        if (prizeWinner == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Prize Winner not found.");
+        } else {
+            return prizeWinner;
+        }
+    }
 }
