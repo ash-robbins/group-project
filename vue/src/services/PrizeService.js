fc2f2d762a2c1aff183ddbcb307bd3341d2bdd3b
Mohammed <mnaser11218@gmail.com>
2023-04-17 05:57:10 +0300

    user can create new prize to backend

---
+++ vue/src/services/PrizeService.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+
+export default {
+
+    // list(){
+    //   return axios.get('/books')
+    // },
+  
+    // getBook(book_id) {
+    //   return axios.get(`/books/${book_id}`)
+    // },
+  
+    // // get reading activity by book_id
+    // getReadingActivity(book_id) {
+    //     return axios.get(`/books/reading_activity/${book_id}`)
+    // },
+  
+    // postBook(book) {
+    //   return axios.post('/postbook', book)
+    // },
+  
+    // updateReadingActivity(reading_activity){
+    //   return axios.put(`/books/reading_activity/${reading_activity.book_id}`, reading_activity)
+    //   }
+  
+    postPrize(prize) {
+        return axios.post('/prize', prize)
+    },
+  
+  
+  
+  
+  
+  
+  
+  
+  }
