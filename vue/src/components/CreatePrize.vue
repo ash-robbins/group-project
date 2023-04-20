8f7e31c673529c3b4ed68524f76dd02ee74c0574
Yae Lee <peteryaelee@gmail.com>
2023-04-20 04:42:43 -0400

    container box for prize forms

--- vue/src/components/CreatePrize.vue
+++ vue/src/components/CreatePrize.vue
@@ -3,13 +3,13 @@
     <!-- <button v-if="updateButton" v-on:click.prevent="updateButton = false">Set new Prize</button> -->
     <button id="update-button" v-if="updateButton" v-on:click.prevent="updateButton = false">Set new Prize</button>
       <form v-else v-on:submit.prevent="postPrize">
-        <label for="prize-description">Prize Description</label>
+        <label for="prize-description">Prize Description:  </label>
         <input id="prize description" class="prize-input" type="text" placeholder="Prize Description" v-model="prize.description"/><br>
         
-        <label for="prize-startdate">Start Date</label>
+        <label for="prize-startdate">Start Date:  </label>
         <input id="prize-startdate" class="prize-input" type="date" v-model="prize.startDate"/> <br>
         
-        <label for="prize-enddate">End Date</label>
+        <label for="prize-enddate">End Date:  </label>
         <input id="prize-enddate" class="prize-input" type="date" v-model="prize.endDate"/><br>
         
         <input id="submit_prize" type="submit" value="Submit"/>
@@ -73,9 +73,26 @@ export default {
   box-shadow: 0 8px 24px 0 rgba(16,39,112,.2);
 }
 
-
+.prizeform{
+  display: flex;
+ 
+  justify-content: center;
+  margin-top: 50px;
+  
+  
+  background-size: cover;
+  background-position: center;
+  border-radius: 15px;
+  margin-bottom: 1rem;
+  background-color: #D3D3D3;
+  padding: 1rem; 
+  border-radius: 15px;
+  
+  
+  
+}
  .prize-input {
-  width: 40%;
+  /* width: 40%;
   height: 40px;
   border: 1px solid black;
   padding: 0 20px;
@@ -85,8 +102,20 @@ export default {
   font-size: 14px;
   transition: all 0.5s ease;
   margin-top: 5px;
-  color: black;
-    
+  color: black; */
+  
+    width: 90%;
+    height: 40px;
+    border: 1px solid white;
+    background: rgb(226, 226, 226);
+    padding: 0 20px;
+    border-radius: 15px;
+    outline: none;
+    font-weight: 400;
+    font-size: 14px;
+    transition: all 0.5s ease;
+    margin-top: 5px; 
+ 
 }
 
 
